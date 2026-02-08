9d6e241d01771f6b9ba7dd5ab18acc13d896c0a6
Abdul Mannan <mannan4u3@gmail.com>
2026-02-09 01:44:09 +0500

    initial commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const login = async (req, res) => {
+  try {
+    res.send("Login route");
+  } catch (error) {}
+};
+
+export const register = async (req, res) => {
+  try {
+    res.send("Register route");
+  } catch (error) {}
+};
---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
